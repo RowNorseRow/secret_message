7d4a3bfe4e4a33551e03efada93b3e906f7e0266
Allen Ellis <diamondstateellis@gmail.com>
2018-02-03 15:57:01 -0600

    Step 2: Rename the existing files pulled for the dir

--- rename_files.py
+++ rename_files.py
@@ -1,6 +1,14 @@
 import os
 def rename_files():
-    file_names = os.listdir('C:\Users\Owner\Documents\prank')
-    print(file_names)
+    file_list = os.listdir('C:\Users\Owner\Documents\prank')
+    print(file_list)
+    os.chdir('C:\Users\Owner\Documents\prank')
+
+    #rename old file names with the code below
+
+    for file_name in file_list:
+        print("Old Name - " + file_name)
+        print("New Name - " + file_name.translate(None, "0123456789"))
+        os.rename(file_name, file_name.translate(None, "0123456789"))
 
 rename_files() 
